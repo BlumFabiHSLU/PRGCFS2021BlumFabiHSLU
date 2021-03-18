7cfa58146fb7351c5b3cbd43a6f750e69efdcc1b
Fabian <fabian.blum@stud.hslu.ch>
2021-03-18 18:53:06 +0100

    update 18.03.

--- 40_Exercises/04_CLA_Operators/logicOperators.c
+++ 40_Exercises/04_CLA_Operators/logicOperators.c
@@ -1,15 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[] )
 {
-	//fall NICHT 3 Argumente erhalten, dann hier ins if (7-10)
-	if( argc != 3 ){
+	//fall NICHT 4 Argumente erhalten, dann hier ins if (7-10)
+	if( argc != 4 ){
 	//input not valid
 	printf("Usage:\n");
-	printf("logicOperators Input1 Input2\n");
+	printf("logicOperators Input1 Input2 Input3\n");
 	}
 	else
-	{	//input is valid -->genau drei Argumente erhalten
+	{	//input is valid -->genau drei + eins Argumente erhalten
+		int val1 = atoi(argv[1]);
+		int val2 = atoi(argv[2]);
+		int val3 = atoi(argv[3]);
+		int sum = 0;
+		float average = 0.0;
+		
+		printf("%d, %d, %d\n", val1, val2 ,val3);
+		
+		if( !(val1 > 0) || !(val2 > 0) || !(val3 > 0))
+		{
+			//Fehlerfall
+			printf("All inputs must be strictly greater than 0\n");
+		}
+		else
+		{
+			if(val1 >= val2 && val1 >= val3 )
+			{
+				//val1 is the greatest
+				printf("%d is the greatest\n", val1);
+			}
+			else if(val2 >= val1 && val2 >= val3 )
+			{
+			//val2 is the greatest
+				printf("%d is the greatest\n", val2);
+			}
+			else
+			{
+			//val3 is the greatest
+				printf("%d is the greatest\n", val3);
+			}
+			
+			//find the smallest
+			if(val1 <= val2 && val1 <= val3 )
+			{
+				//val1 is the smallest
+				printf("%d is the smallest\n", val1);
+			}
+			else if(val2 <= val1 && val2 <= val3 )
+			{
+			//val2 is the smallest
+				printf("%d is the smallest\n", val2);
+			}
+			else
+			{
+			//val3 is the smallest
+				printf("%d is the smallest<n", val3);
+			}
+			
+			//compute the sum
+			sum = val1 + val2 + val3;
+			printf("The sum is: %d\n", sum);
+			
+			//compute the average
+			average = (float)sum/3.0;
+			printf("The average  is: %2f\n", average);
+		}	
+	}
+		
+#if 0 //Auskommentieren mit Präprozessor		
+	
+	
+		if
+		
+		
+		
 		printf("number of input arguments: %d\n",argc );
 	
 		// nimm eine int variable i und starte mit 0
@@ -27,6 +93,7 @@ int main(int argc, char *argv[] )
 		
 		
 	}
+#endif
 	return 0;
 
 }
